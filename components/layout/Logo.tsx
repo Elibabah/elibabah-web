9ff7b1f47eef258bace2de552d22f46529b222c8
Elibabah <delias16.hernandez@gmail.com>
2026-08-17 18:42:50 +1200

    feat: cover logo

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -6,19 +6,28 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useTheme } from "next-themes";
 
-export function Logo() {
+type LogoProps = {
+  includesName?: boolean;
+}
+
+export function Logo({ includesName = false }: Readonly<LogoProps>) {
   const { resolvedTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => { setMounted(true); }, []);
 
   return (
-    <Link href="/" aria-label="Home">
+    <Link href="/" aria-label="Home" className="flex items-center justify-between gap-2.75">
       <img
         src={mounted && resolvedTheme === "dark" ? "/logo-dark.svg" : "/logo-light.svg"}
         alt="Elibabah"
         className="h-8 w-auto"
       />
+      {includesName &&
+        <div className="font-heading font-bold text-[21px] tracking-[-0.3px] text-foreground hover:text-foreground">
+          elibabah
+        </div>
+      }
     </Link>
   );
 }
